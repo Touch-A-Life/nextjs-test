d23264e74448ca7f2e6bca1b8225a6b0cb79c16c
vignesh jogi <vignesh.j@touchalife.org>
2023-08-31 09:52:10 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+        <Link href="/posts/first-post">← Back to home</Link>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      // props for your component
+    },
+  };
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout >
+      <h1>Hi</h1>
+      
+    </Layout>
+  );
+}
